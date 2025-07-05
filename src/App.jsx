e3884595f7a4f8d29829f313768c9708b18a6c7b
kien1234aa <nguyentrungkien382004@gmail.com>
2025-07-05 13:18:07 +0700

    seachpage

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import {
   createBrowserRouter,
   RouterProvider,
-} from "react-router";
+} from "react-router-dom";
 import './App.css'
 import Layout from "./component/Layout/Layout";
 import HomePage from "./pages/HomePage/HomePage";
@@ -14,21 +14,22 @@ import Register from "./pages/Register/Register";
 import Login from "./pages/Login/Login";
 
 function App() {
-
   const router = createBrowserRouter([
     {
       path: "/",
       Component: Layout,
       children: [
         { index: true, Component: HomePage },
-        { path: 'search', Component: Search },
         { path: 'candidate', Component: Candidate },
         { path: 'job-detail', Component: JobDetail },
         { path: 'register', Component: Register },
         { path: 'login', Component: Login },
       ]
     },
-
+    {
+      path: "/Search",
+      Component: Search,
+    },
 
   ]);
 
